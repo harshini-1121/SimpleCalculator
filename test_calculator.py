f549a9fee569fdf235f5d9da5b707c5934b2d6c1
harshini-1121 <71762333015@cit.edu.in>
2026-02-27 15:07:19 +0530

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -4,7 +4,7 @@ from main import add
 class TestCalculator(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(3, 3), 6)
 
 if __name__ == "__main__":
     unittest.main()
